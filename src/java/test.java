6372ad4fbe70683e9a781f2d72a4b04c404bf027
quyenpt <quyenpt@dotohsoft.com>
2018-04-03 12:01:08 +0700

    nutch 1.8

---
+++ src/java/test.java
@@ -0,0 +1,7 @@
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("世界您好!");
+		String a = "&#12395;&#12424;&#12427;&#26085;&#26412;&#35486;&#33258;&#28982;&#35328;&#35486;&#20966;&#29702";
+	}
+}
